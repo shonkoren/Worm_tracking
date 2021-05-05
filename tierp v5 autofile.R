7575e96dec98bac4783ca2139ac819eef82b0b32
shkoren <shon.a.koren@gmail.com>
2021-05-04 22:28:58 -0400

    Update tierp v5 autofile.R

--- tierp v5 autofile.R
+++ tierp v5 autofile.R
@@ -8,13 +8,13 @@ library(tidyquant)
 
 #set wd
 #setwd("C:/Users/shona/OneDrive/Desktop/n2 vs 269/n2 vs apw269/final")
-setwd("D:/APW Lab/Food ON OFF/trial 3/Results")
+setwd("D:/APW Lab/Food ON OFF/trial 5/Together")
 temp = intersect(list.files(pattern = "\\.hdf5$", full.names = T), list.files(pattern = "featuresN", full.names = T))
 
 my_data = list()
 meta = ''
 df = ''
-
+worms = list()
 
 for (i in unique(temp)){
   meta <- h5read(file = i, name = "trajectories_data")
@@ -28,59 +28,14 @@ for (i in unique(temp)){
   df = df %>% mutate_at(vars(worm_index), funs(meta$worm_index_manual[match(., meta$worm_index_joined)]))
   df = df %>% select(1:4)
   df$file.ID <- i
+  worms[[i]] = df %>% distinct(worm_index) %>% pull(worm_index)
   my_data[[i]] = df
   
 }
 
 h5closeAll()
-
-
-#TEST
-n2.hd5 = "rb off 1_featuresN.hdf5"
-n22.meta = h5read("rb off 1_featuresN.hdf5", "trajectories_data")
-n22.data = h5read("rb off 1_featuresN.hdf5", "timeseries_data")
-h5closeAll()
-n22.meta.f = n22.meta %>% filter(worm_label == 1)
-n22.data.f = n22.data %>% filter(worm_index %in% n22.meta.f$worm_index_joined)
-n22.data.f %>% pull(worm_index) %>% unique()
-n22.data %>% pull(worm_index) %>% unique()
-my_data[[7]] %>% pull(worm_index) %>% unique()
-
-# # 
-# # #load hd5 files
-# # #Here, we had 4 trials, I just deleted the first trial. Variables labeled n (for n2 WT worms) + trial#
-# n2.hd5 = "n2 food 2_featuresN.hdf5"
-# # n23.hd5 = "n2 3_featuresN.hdf5"
-# # n24.hd5 = "n2 4_featuresN.hdf5"
-# # 
-# # #Extract metadata with manual curated worms and movement data for timeseries per worm
-# n22.meta = h5read("n2 food 2_featuresN.hdf5", "trajectories_data")
-# n22.data = h5read("n2 food 2_featuresN.hdf5", "timeseries_data")
-# # n23.meta = h5read("n2 3_featuresN.hdf5", "trajectories_data")
-# # n23.data = h5read("n2 3_featuresN.hdf5", "timeseries_data")
-# # n24.meta = h5read("n2 4_featuresN.hdf5", "trajectories_data")
-# # n24.data = h5read("n2 4_featuresN.hdf5", "timeseries_data")
-# 
-# #close all files just in case
-# h5closeAll()
-# 
-# #Trim data to only manually curated worms
-# n22.meta.f = n22.meta %>% filter(worm_label == 1)
-# n22.data.f = n22.data %>% filter(worm_index %in% n22.meta.f$worm_index_joined)
-# n23.meta.f = n23.meta %>% filter(worm_label == 1)
-# n23.data.f = n23.data %>% filter(worm_index %in% n23.meta.f$worm_index_joined)
-# n24.meta.f = n24.meta %>% filter(worm_label == 1)
-# n24.data.f = n24.data %>% filter(worm_index %in% n24.meta.f$worm_index_joined)
-# 
-# #Limit actual timestamp data to only those with manual curation
-# n22.data.f = n22.data.f %>% mutate_at(vars(worm_index), funs(n22.meta.f$worm_index_manual[match(., n22.meta.f$worm_index_joined)]))
-# n23.data.f = n23.data.f %>% mutate_at(vars(worm_index), funs(n23.meta.f$worm_index_manual[match(., n23.meta.f$worm_index_joined)]))
-# n24.data.f = n24.data.f %>% mutate_at(vars(worm_index), funs(n24.meta.f$worm_index_manual[match(., n24.meta.f$worm_index_joined)]))
-# 
-# #Pull individual worms
-# n22.data.f %>% pull(worm_index) %>% unique()
-# n23.data.f %>% pull(worm_index) %>% unique()
-# n24.data.f %>% pull(worm_index) %>% unique()
+agg = data.frame(rbindlist(my_data)) %>% 
+  mutate(ROI = stringr::str_extract(Label, "(?<=nd2:)(.*)(?=:c:)"))
 
 ##Filter for movement, add time instead of frame, correct for fps diff, label genos or trials
 n22.data.ff = n22.data.f %>% select(1:4) %>% filter(!between(timestamp, 0, 48)) %>% mutate(time = timestamp / 48) %>% mutate(trial = "WT 1") %>% mutate(geno = 'WT')
