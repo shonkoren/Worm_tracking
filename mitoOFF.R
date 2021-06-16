462fd124aaf848ec1c422be57e4b0bf93b466f80
shkoren <shon.a.koren@gmail.com>
2021-06-16 16:41:49 -0400

    Update mitoOFF.R

--- mitoOFF.R
+++ mitoOFF.R
@@ -8,27 +8,29 @@ library(cowplot)
 library(ggstatsplot)
 library(tidyquant)
 
-setwd("D:/APW Lab/Anezka/APW208/vids/Results")
-temp = intersect(list.files(pattern = "\\.hdf5$", full.names = T), list.files(pattern = "features", full.names = T))
+#for /f "tokens=*" %a in ('dir /b') do ren "%a" "u%a"
 
+{ setwd("D:/APW Lab/Anezka/APW208/Anezka_redo/Tierpsy-210615/Results")
+temp = intersect(list.files(pattern = "\\.hdf5$", full.names = T), list.files(pattern = "features", full.names = T))
 my_data = list()
 my_data2= list()
 meta = ''
 df = ''
 df2 = ''
 dfx = ''
-worms = list()
+worms = list() }
 
 for (i in unique(temp)){
+  print(i)
   df = h5read(file = i, name = "features_means")
   df = as.data.frame(df)
   #add column with i
   dfx = df
-  df = df %>% select(1:3, 5, 694, 661, 708) %>% filter(as.numeric(n_valid_skel) > 300)
+  df = df %>% select(1:3, 5, 694, 661, 708) %>% filter(as.numeric(n_valid_skel) > 0)
   df$file.ID <- i
   worms[[i]] = df %>% distinct(worm_index) %>% pull(worm_index)
   my_data[[i]] = df
-  if (i == './nf4_features.hdf5') {df.nf4 = as.data.frame(df)}
+  # if (i == './ALDP-210603_features.hdf5') {df.nf4 = as.data.frame(df)}
   
   df2 = h5read(file = i, name = 'features_timeseries')
   df2 = as.data.frame(df2)
@@ -59,7 +61,8 @@ h5closeAll()
                              ATR == 'unt' & light == 'Dark' & food == 'Off-Food' & starve == 'Pre' ~ 'A-L-F-',
                              ATR == 'unt' & light == 'Dark' & food == 'Food' & starve == 'Pre' ~ 'A-L-F+',
                              ATR == 'unt' & light == 'Light' & food == 'Off-Food' & starve == 'Pre' ~ 'A-L+F-',
-                             ATR == 'unt' & light == 'Light' & food == 'Food' & starve == 'Pre' ~ 'A-L+F+'))
+                             ATR == 'unt' & light == 'Light' & food == 'Food' & starve == 'Pre' ~ 'A-L+F+')) %>%
+    filter(file != 'ALFP1-210603')
   agg$omega_turns_frequency[is.nan(agg$omega_turns_frequency)]=0
   agg$upsilon_turns_frequency[is.nan(agg$upsilon_turns_frequency)]=0
   agg = agg %>% mutate(omega = omega_turns_frequency * n_frames, upsilon = upsilon_turns_frequency * n_frames)
@@ -70,13 +73,14 @@ avg.agg = agg %>% group_by(group, worm_ID) %>%
             meanU = mean(upsilon), meanUF = mean(upsilon_turns_frequency), meanD = mean(worm_dwelling),
             meanPC = mean(path_curvature), mean.absPC = mean(path_curvature)) %>% as.data.frame()
 }
-# write.csv(avg.agg, 'avg.agg.csv')
+write.csv(avg.agg, 'avg.agg600f.filterbad.csv')
 
 {
   pof = ggstatsplot::ggbetweenstats(data = avg.agg, x = group, y = meanOF, type = 'np', 
                                     pairwise.display = "significant", p.adjust.method = "holm", 
                                     bf.message = FALSE, ylab = "Omega Turn Frequency (1/s)", xlab = '')
-  pof }
+  pof
+  }
   
 agg2 = data.frame(rbindlist(my_data2)) %>%
   mutate(file = stringr::str_extract(file.ID, "(?<=/)[^_]+")) %>% 
@@ -98,15 +102,19 @@ agg2 = data.frame(rbindlist(my_data2)) %>%
                            ATR == 'unt' & light == 'Light' & food == 'Food' & starve == 'Pre' ~ 'A-L+F+')) %>%
   mutate(skeleton_fit = case_when(skeleton_id == -1 ~ NaN, TRUE ~ skeleton_id)) %>%
   mutate(worm_ID = paste0(worm_index,".",file)) %>% group_by(worm_ID) %>% arrange(worm_ID, timestamp) %>% na.omit() %>%
-  mutate(difference_f = timestamp - lead(timestamp),
+  mutate(difference_f = timestamp - lag(timestamp),
          difference_mm = motion_modes - lead(motion_modes)) %>%
-  mutate(reversal = case_when(difference_f <10 & difference_mm == -2 ~ 1,
+  mutate(reversal = case_when(difference_f <10 & difference_mm == 2 ~ 1,
                               TRUE ~ 0)) %>%
   mutate(RevNum = sum(reversal)) %>% arrange(group, file, worm_ID, timestamp) %>% na.omit() %>% select(c(12,14,18)) %>% distinct()
 
 agg.agg = left_join(agg, agg2) %>% ungroup() %>% filter(!is.na(RevNum)) %>% mutate(RevFreq = (10 * RevNum) / n_frames)
 # ggplot(agg2, aes(x = timestamp, y = reversal)) + geom_line() + facet_wrap(~ group)
 prf = ggstatsplot::ggbetweenstats(data = agg.agg, x = group, y = RevFreq, 
-                                  pairwise.display = "all", p.adjust.method = "fdr", bf.message = FALSE,
+                                  pairwise.comparisons = F, p.adjust.method = "fdr", bf.message = FALSE,
                                   xlab = "Group", ylab = "Reversal Frequency (1/s)")
 prf
+ggplot(agg2, aes(x = group, y = RevNum)) + geom_boxplot()
+write.csv(agg.agg, 'agg.agg.csv')
+
+
